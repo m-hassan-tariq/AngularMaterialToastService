59c9eaf0566463ab3aa706abc77f9d475d7316af
Muhammad Hassan <hassantariq.8631@gmail.com>
2016-02-26 15:48:07 -0600

    Initial

---
+++ Scripts/app/app.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', ['ngMaterial']);
+
+    app.controller('toastController', function (toastAlert) {
+
+        var vm = this;
+        
+        vm.defaultToaster = function () {
+            toastAlert.defaultToaster('Toast Activated');
+        };
+
+        vm.customToaster = function () {
+            toastAlert.customToaster('Toast Activated', '3000', 'bottom left');
+        };
+    });
+
+})();
---
+++ Scripts/app/toastAlertService.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('toastAlert', toastAlert);
+
+    toastAlert.$inject = ['$mdToast'];
+
+    function toastAlert($mdToast) {
+
+        var service = {
+            customToaster: customToaster,
+            defaultToaster: defaultToaster
+        };
+
+        return service;
+
+        ////////////////////////////Implementation//////////////////////////////////////
+
+        function customToaster(content , delay, position) {
+            $mdToast.show(
+              $mdToast.simple()
+                .textContent(content)
+                .position(position)
+                .hideDelay(delay)
+            );
+        }
+
+        function defaultToaster(content) {
+            $mdToast.show(
+              $mdToast.simple()
+                .textContent(content)
+                .position('bottom right')
+                .hideDelay(3000)
+            );
+        }
+
+    }
+
+})();
